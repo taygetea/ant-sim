5485a605f621b16c79d2887413987cdf889d7dc8
Olivia Schaefer <taygetea@gmail.com>
2012-03-09 03:13:50 -0500

    ant

--- World.py
+++ World.py
@@ -1,2 +1,12 @@
+import random
+
 class World:
     def __init__(self):
+        # place the nest and the food
+
+
+    def tick(self)
+        # call ant.move
+        # call ant.loc, cross-check with nest, food, and pheremone
+        # tell ant if it's on any world elements
+        # increment the clock
