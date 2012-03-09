a82388c8671efd9e1d65b961b7f106f5eda3ab3e
Olivia Schaefer <taygetea@gmail.com>
2012-03-09 02:46:34 -0500

    ant

--- ant.py
+++ ant.py
@@ -1,8 +1,12 @@
 class Ant:
-    def __init__(self,x,y):
+    def __init__(self,x,y,home):
         self._x = x
         self._y = y
         self._mode = 0
+        self._home = home
 
 
-    def location(self)
+    def status(self,mode,x,y)
+        self._mode = mode
+        self._x = x
+        self._y = y
